e27f9723a040717c4e862823c8f5355115898454
LaszloHerczeg <laszloherczeg96@gmail.com>
2024-12-14 13:53:13 +0100

    Update sum_of_four_values.py

--- 1.feladat/sum_of_four_values.py
+++ 1.feladat/sum_of_four_values.py
@@ -20,15 +20,15 @@ def sum_of_four_values(n:int, x:int, array:list):
         
         target = x - sorted_array[i][0]
         # Kiválasztom a 2. elemet:
-        for sub_i in range(i + 1, n - 2):
-            sub_target = target - sorted_array[sub_i][0]
+        for j in range(i + 1, n - 2):
+            sub_target = target - sorted_array[j][0]
             # Kiválasztom a 3. és a 4. elemet:
-            left = sub_i + 1
+            left = j + 1
             right = n - 1
             while left < right:
                 # Ha az összeg egyenlő a megcélzott értékkel, akkor visszaadom az egészek pozícióját:
                 if sorted_array[left][0] + sorted_array[right][0] == sub_target:
-                    return (sorted_array[i][1], sorted_array[sub_i][1], sorted_array[left][1], sorted_array[right][1])
+                    return (sorted_array[i][1], sorted_array[j][1], sorted_array[left][1], sorted_array[right][1])
                 # Ha a kapott érték túl alacsony, akkor a 3. elem indexét növelem:
                 elif sorted_array[left][0] + sorted_array[right][0] < sub_target:
                     left += 1
